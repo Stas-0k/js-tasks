2d7064cef85cdb879f143755986cb0e4c7c03cb3
Stas-0k <brickbybrick15@gmail.com>
2022-09-16 15:31:18 +0300

    commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//module 3 task 19
+
+/*const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+
+
+function getAllPropValues(propName) {
+  // Change code below this line
+    let array = []; 
+    for (const product of products) { 
+        if (propName === "name") {
+            array.push(product.name);
+        } else if (propName === "price") {
+            array.push(product.price);
+        } else if (propName === "quantity") {
+            array.push(product.quantity);
+        } 
+        
+    }
+    return array;
+  // Change code above this line
+}
+
+
+console.log(getAllPropValues("price"));*/
+
+
+//module 3 task 20
+
+/*const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+    for (const product of products) { 
+        if (productName === product.name) {
+            return product.price * product.quantity;
+        } 
+            
+        
+    }
+return 0
+
+  // Change code above this line
+}
+
+console.log(calculateTotalPrice("sds"));*/

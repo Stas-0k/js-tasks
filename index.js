baabbb436b4afd8a81ae5fddb3fc64413d4dda81
Stas-0k <brickbybrick15@gmail.com>
2022-09-26 19:13:22 +0300

    commit

--- index.js
+++ index.js
@@ -432,3 +432,19 @@ console.log(makeMessage('Royal Grand', makePizza));*/
 
 //module 4 task 3
 
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+
+
+
